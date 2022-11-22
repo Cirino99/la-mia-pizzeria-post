246f321ee4e7aba6bdc41f7bf5562beda63851e7
Simone <cirino.simone99@gmail.com>
2022-11-22 12:00:21 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,25 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            List<Pizza> pizze = db.Pizze.ToList();
+            return View(pizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            Pizza pizza = db.Pizze.Where(p => p.Id == id).FirstOrDefault();
+            if(pizza == null)
+                return View("NotFound","La pizza cercata non è stata trovata");
+            return View(pizza);
+        }
+    }
+}
---
+++ Data/PizzeriaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Data
+{
+    public class PizzeriaDbContext : DbContext
+    {
+        public DbSet<Pizza> Pizze { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-pizzeria;Integrated Security=True;Encrypt=false;");
+        }
+    }
+}
